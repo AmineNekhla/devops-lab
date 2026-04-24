07761e875a64cb639f9b8534ac6656ccd5759e93
AmineNekhla <amine.nekhla3@gmail.com>
2026-04-24 10:02:30 +0200

    'update'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 10 - Déployée automatiquement"
+    return "Version 20 - Déployée automatiquement"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
